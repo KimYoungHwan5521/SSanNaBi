b71102bac5898efcf69f3c54a5df925d846f855d
KimYoungHwan <aorkslrdnjwm@naver.com>
2024-04-08 13:57:00 +0900

    implement Supervisor death timer(visual only)

--- Assets/MyAssets/Scripts/AI/AISupervisor.cs
+++ Assets/MyAssets/Scripts/AI/AISupervisor.cs
@@ -1,30 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using static UnityEditor.PlayerSettings;
 
 public class AISupervisor : MonoBehaviour
 {
     public SpriteRenderer eye;
+    public TextMeshProUGUI deathTimerText;
+
     Transform playerT;
     Vector3 preferPosition;
     Vector3 preferDirection;
+    RectTransform canvas;
+    float xPos;
+    float yPos;
+    Vector2 pos;
+
+    public float deathTime;
+    float curDeathTime;
 
     public float moveSpeed;
+    bool isWatching;
 
     private void Start()
     {
         playerT = GameObject.FindGameObjectWithTag("Player").transform;
+        canvas = GetComponentInChildren<Canvas>().GetComponent<RectTransform>();
+        curDeathTime = deathTime;
+    }
+
+    private void Update()
+    {
+        if(isWatching)
+        {
+            deathTimerText.enabled = true;
+            deathTimerText.text = $"{(int)curDeathTime}";
+            xPos = Mathf.Clamp(Camera.main.WorldToScreenPoint(transform.position).x, 0, canvas.rect.width - 10 - deathTimerText.rectTransform.rect.width);
+            yPos = Mathf.Clamp(Camera.main.WorldToScreenPoint(transform.position).y, deathTimerText.rectTransform.rect.height, canvas.rect.height - 10 - deathTimerText.rectTransform.rect.height);
+            pos = new Vector2(xPos, yPos);
+            deathTimerText.transform.position = pos;
+            curDeathTime -= Time.deltaTime;
+        }
+        else
+        {
+            deathTimerText.enabled = false;
+            curDeathTime = deathTime;
+        }
     }
 
     void FixedUpdate()
     {
         if(FindPlayer())
         {
+            isWatching= true;
             preferPosition = playerT.position;
             eye.material.color = new Color(1, Mathf.Clamp(eye.material.color.g - 3 * Time.fixedDeltaTime, 0, 1), 0);
         }
         else
         {
+            isWatching= false;
             eye.material.color = new Color(1, Mathf.Clamp(eye.material.color.g + Time.fixedDeltaTime, 0, 1), 0);
 
         }
@@ -40,11 +75,9 @@ public class AISupervisor : MonoBehaviour
         Physics2D.RaycastNonAlloc(playerT.position, transform.forward, hits, 2f, LayerMask.GetMask("Default"));
         for(int i=0; i<hits.Length; i++)
         {
-            Debug.Log(hits[i].collider);
             if (hits[i].collider == null) return true;
             if (hits[i].collider.CompareTag("BackWall"))
             {
-                Debug.Log(hits[i].collider.tag);
                 return false;
             }
         }
