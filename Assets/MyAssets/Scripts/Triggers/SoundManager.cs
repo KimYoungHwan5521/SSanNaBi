3c5bee7be9e1a896e92567516701e31edc999fae
KimYoungHwan <aorkslrdnjwm@naver.com>
2024-04-24 17:03:25 +0900

    implement volume

--- Assets/MyAssets/Scripts/Triggers/SoundManager.cs
+++ Assets/MyAssets/Scripts/Triggers/SoundManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SoundManager : MonoBehaviour
 {
@@ -11,6 +12,9 @@ public class SoundManager : MonoBehaviour
     public static AudioClip[] bgms;
     static AudioSource audioSource;
     static int playingBgmIndex;
+
+    GameObject Volume;
+    GameObject VolumeInst;
 
     public void Awake()
     {
@@ -27,9 +31,36 @@ public class SoundManager : MonoBehaviour
 
     private void Start()
     {
+        Volume = Resources.Load<GameObject>("Prefabs/UI/Volume");
+
         audioSource = GetComponent<AudioSource>();
         bgms = clips;
         PlayBgm(0);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(VolumeInst == null)
+            {
+                VolumeInst = Instantiate(Volume, GameObject.FindGameObjectWithTag("MainCanvas").transform);
+                VolumeInst.GetComponentInChildren<Slider>().value = audioSource.volume;
+            }
+            else if(VolumeInst.activeSelf)
+            {
+                VolumeInst.SetActive(false);
+            }
+            else 
+            {
+                VolumeInst.SetActive(true); 
+            }
+        }
+        if(VolumeInst!= null)
+        {
+            audioSource.volume = VolumeInst.GetComponentInChildren<Slider>().value;
+
+        }
     }
 
     public static void PlayBgm(int bgmIndex)
